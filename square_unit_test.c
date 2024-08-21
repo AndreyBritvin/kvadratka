662bad0d3db9f4ffeadc94feecc934ffe632a418
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-08-21 17:53:31 +0300

    Check in unit test

--- square_unit_test.c
+++ square_unit_test.c
@@ -2,28 +2,38 @@
 
 struct unit_test_input all_tests[] =
 {
-    {{0, 0, 0}, {0, 0, 0}},
-    {{0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {INF_ROOTS, 0, 0}},
+    {{0, 0, 3.14159265}, {ZERO_ROOTS, 0, 0}},
     {{0, 0, 0}, {0, 0, 0}},
     {{0, 0, 0}, {0, 0, 0}},
 };
 
 int run_test(int test_id, struct unit_test_input input)
 {
-    double x1 = 0, x2 = 0;
+    struct solution sol_to_check = {0, 0, 0};
 
-    printf("%lg, %lg\n", input.expected_solution.x1, input.expected_solution.x2);
+    solve_equation(input.coefficients, &sol_to_check);
+
     set_minimum_solution(&input.expected_solution.x1, &input.expected_solution.x2);
-    printf("%lg, %lg\n", input.expected_solution.x1, input.expected_solution.x2);
+    set_minimum_solution(&sol_to_check.x1,            &sol_to_check.x2           );
 
-    //solve_equation();
+    if (input.expected_solution.n_roots != sol_to_check.n_roots ||
+        !compare_equal_double(input.expected_solution.x1, sol_to_check.x1) ||
+        !compare_equal_double(input.expected_solution.x1, sol_to_check.x1))
+    {
+        printf("ERROR in Test #%d: a=%lg b=%lg c=%lg n_roots=%d x1=%lg x2=%lg\n"
+               "Expected values: n_roots=%d x1=%lg x2=%lg\n",
+               test_id, input.coefficients.a, input.coefficients.b, input.coefficients.c,
+               sol_to_check.n_roots, sol_to_check.x1, sol_to_check.x2,
+               input.expected_solution.n_roots, input.expected_solution.x1, input.expected_solution.x2);
+    }
 
     return 0;
 }
 
 int run_all_tests()
 {
-    for (int test_id = 0; test_id < sizeof(all_tests) / sizeof(struct unit_test_input); test_id++)
+    for (int test_id = 1; test_id < sizeof(all_tests) / sizeof(struct unit_test_input) + 1; test_id++)
     {
         run_test(test_id, all_tests[test_id]);
     }
