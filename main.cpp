39f29c07b7c560eea5ede61f8bfa529bff36c9ac
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-08-22 14:06:06 +0300

    Fix No Flag

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc == 1)
+    {
+        wrong_flag(argv[0], "");
+    }
+
     for (int arg_num = 1; arg_num < argc; arg_num++)
     {
         parse_mode(argv[0], argv[arg_num]);
