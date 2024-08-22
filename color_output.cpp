7c233ac5133316e256800fbb58e359e0387240d1
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-08-22 16:48:08 +0300

    Added more colors

--- color_output.cpp
+++ color_output.cpp
@@ -7,9 +7,43 @@ void print_color(int color, const char * format, ...)
     va_list args;
     va_start (args, format);
 
-    printf("\033[0m");
+    set_color(color);
+
     vprintf(format, args);
-    printf("\033[0m");
+    reset_color();
 
     va_end (args);
+}
+
+void set_color(int color)
+{
+    switch (color)
+    {
+        case RED:
+        {
+            set_red_color();
+            break;
+        }
+
+        default:
+        {
+            reset_color();
+            break;
+        }
+    }
+}
+
+void reset_color()
+{
+    printf("\033[0m");
+}
+
+void set_red_color()
+{
+    printf("\033[0;31m");
+}
+
+void set_green_color()
+{
+    printf("\033[0;32m");
 }
--- color_output.h
+++ color_output.h
@@ -6,9 +6,21 @@
 
 enum color
 {
-    RED
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    PURPLE,
+    CYAN,
+    WHITE,
 };
 
 void print_color(int color, const char * format, ...);
+
+void set_color(int color);
+void set_red_color();
+void set_green_color();
+void reset_color();
 
 #endif // COLOR_OUTPUT_H_
