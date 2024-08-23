6005236cd83cf9a636d82472995f95bd5bc64675
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-08-23 12:51:52 +0300

    Update my_assert.h

--- include/my_assert.h
+++ include/my_assert.h
@@ -1,10 +1,13 @@
 #ifndef MY_ASSERT_H_
 #define MY_ASSERT_H_
 
-#include "color_output.h"
+#include <stdio.h>
 
-#define MY_ASSERT(EXPR); {((EXPR)) ?  (void(0)) :\
-                          print_color(RED, "ERROR in %s line %d (function %s):"\
-                           "Assert "#EXPR" failed\n", __FILE__, __LINE__, __func__); }
+#define MY_ASSERT(EXPR); {((EXPR)) ?  true :\
+                          fprintf(stderr, "\033[0;31m"\
+                            "ERROR in %s line: %d (function %s):\n"\
+                            "Assertion "#EXPR" failed\n\n" "\033[0m", \
+                            __FILE__, __LINE__, __func__); }
+
 
 #endif // MY_ASSERT_H_
