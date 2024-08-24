5d83d5edd5102524394efad68721f5f03e1ef44d
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-08-24 12:18:43 +0300

    Begin input output file

---
+++ include/file_input.h
@@ -0,0 +1,18 @@
+/**
+ * @file file_input.h
+ * @author Andrey Britvin
+ * @brief Работа с файловым вводом выводом
+ * @version 1.0
+ * @date 2024-08-24
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+
+#ifndef FILE_INPUT_H_
+#define FILE_INPUT_H_
+
+
+
+#endif // FILE_INPUT_H_
